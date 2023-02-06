a38140384c1a526b1f6e8d5937ea5ec7e71a9a74
Paul MacKay <paul.themac@gmail.com>
2023-02-06 16:26:12 -0700

    cleanup of bazel files in sub directories

--- phase2/cmd/myapp1/internal/app.go
+++ phase2/cmd/myapp1/internal/app.go
@@ -3,9 +3,16 @@ package app
 import (
 	"fmt"
 	"io"
+	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello %s!", r.URL.Path[1:])
+}
+
 func Run(args []string, stdout io.Writer) error {
-	fmt.Println("We are in myapp1")
-	return nil
+	fmt.Println("We are in myapp1 - starting server")
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":8080", nil)
+	return err
 }
